f96aae651cb446cff477a0bb1ea12f74ffc509a0
AUTOBOTS00 <85267187+AUTOBOTS00@users.noreply.github.com>
2021-06-03 08:01:00 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -153,7 +153,7 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔍 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"🔍 Page 1/{len_result if len_result < max_pages else max_pages} 🔍", callback_data="ignore")
         ])
         
         

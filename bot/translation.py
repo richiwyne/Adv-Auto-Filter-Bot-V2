6a2970e8a0c0b3b086bb6dd44739e19fb34eb85b
richiwyne <85574282+richiwyne@users.noreply.github.com>
2021-06-08 23:47:21 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,14 +4,7 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
-
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
-
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
+    START_TEXT = """<b>No One Is Really Going To Help You 😂</b>"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
